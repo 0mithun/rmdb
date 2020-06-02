168dff50e173ebd3657af35146c15630f7094f21
Mithun Halder <mithun.tutorial.one@gmail.com>
2020-06-02 19:10:22 +0600

    Add base name

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,7 +7,7 @@ import Movie from '../Movie/Movie';
 
 const App = ()=>{
     return (
-        <BrowserRouter>
+        <BrowserRouter basename="rmdb">
             <React.Fragment>
                 <Header />
                 <Switch>
